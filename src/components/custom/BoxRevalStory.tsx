d78b43fa1ccbd93e2e30437320006c7b46fd249f
dewank07 <dewankrastogi007@gmail.com>
2025-03-15 13:26:21 +0530

    feat(home): add home page basic structure

---
+++ src/components/custom/BoxRevalStory.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { BoxReveal } from "@/components/magicui/box-reveal";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+export function BoxRevalStory() {
+  return (
+    <div className='max-w-lg items-center justify-center overflow-hidden pt-8'>
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <p className='text-[3.5rem] font-semibold'>
+          Why this project<span className='text-[#5046e6]'>?</span>
+        </p>
+      </BoxReveal>
+
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <h2 className='mt-[.5rem] text-[1rem]'>
+          Once I wanted to watch <span className='text-[#5046e6]'>Ghosted (2023)</span>
+          with someone, and we couldn&apos;t find anywhere, thankfully{" "}
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <span className='underline'>torrent</span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span className='text-xs'>Do not use torrent, I don&apos;t support piracy</span>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>{" "}
+          helped me.
+        </h2>
+        <h2 className='mt-[.5rem] text-[1rem] '>
+          But <span className='text-[#5046e6]'>google meet</span> shares audio when a tab is shared, while sharing
+          screen I have to unmute myself {":("}
+        </h2>
+      </BoxReveal>
+
+      <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <Button className='mt-[1.6rem] bg-[#5046e6]'>This gave me this idea</Button>
+      </BoxReveal>
+    </div>
+  );
+}
---
+++ src/layouts/HomePageLayout.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { BoxRevalStory } from "@/components/custom/BoxRevalStory";
+import { DockBar } from "@/components/custom/Dock";
+import { FeaturesSection } from "@/components/custom/Features";
+import { TerminalStory } from "@/components/custom/TerminalStory";
+import { AuroraText } from "@/components/magicui/aurora-text";
+import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
+import { LineShadowText } from "@/components/magicui/line-shadow-text";
+// import Image from "next/image";
+
+export default function HomePageLayout() {
+  return (
+    <div>
+      <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+        <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+          <h1 className='text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl z-10'>
+            Your friendly{" "}
+            <LineShadowText className='italic' shadowColor={"black"}>
+              In
+            </LineShadowText>{" "}
+            <AuroraText>Browser</AuroraText>
+            <LineShadowText className='italic' shadowColor={"black"}>
+              Player
+            </LineShadowText>{" "}
+          </h1>
+          <div className='flex items-center justify-center w-full'>
+            <InteractiveHoverButton className='bg-transparent'>Let&apos;s watch together ✨</InteractiveHoverButton>
+          </div>
+        </main>
+        <div className='absolute bottom-10 left-0 right-0'>
+          <DockBar />
+        </div>
+      </div>
+      <div className='h-screen flex items-center justify-center px-20'>
+        <FeaturesSection />
+      </div>
+      <div className='h-screen flex items-center justify-center px-20 gap-10'>
+        <BoxRevalStory />
+        <TerminalStory />
+      </div>
+    </div>
+  );
+}
